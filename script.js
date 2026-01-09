98486467c76cda266d22fc67ab1e70aad99fe7af
Alex <8157518@gmail.com>
2026-01-09 20:55:01 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// ================ Game of Life Rules ==========================
+//      For a space that is populated:
+// Each cell with 1 or 0 neighbors dies, as if by solitude.
+// Each cell with >= 4 neighbors dies, as if by overpopulation.
+// Each cell with 2 or 3 neighbors survives.
+//
+//      For a space that is empty or unpopulated:
+// Each cell with three neighbors becomes populated.
+// ==============================================================
